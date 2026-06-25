0c2c75ed8c0a91d32aa17496c2edbc9053446cf7
Botf1shy <141035262+Botf1shy@users.noreply.github.com>
2026-06-25 10:13:14 +0100

    Add files via upload

--- randomwalk.py
+++ randomwalk.py
@@ -5,15 +5,20 @@ import random
 def main():
     step_size = int(input("s:"))
     num_steps = int(input("N:"))
+    num_randomwalks = int(input("Number of random walks:"))
     x, y = 0, 0
     fig, ax = plt.subplots()
     ax.set(xlabel='X', ylabel='Y', title=f'Random Walk: Step Size = {step_size}, Number of Steps = {num_steps}')
     ax.grid()
 
-    for i in range(num_steps):
-        x, y = random_walk(x, y, step_size)
-        ax.plot(x, y, '-o')
-    
+    for i in range(num_randomwalks):
+        color = (random.random(), random.random(), random.random())
+        xs, ys = [x], [y]
+        for _ in range(num_steps):
+            x, y = random_walk(x, y, step_size)
+            xs.append(x); ys.append(y)
+            ax.plot(xs[-2:], ys[-2:],'-o', color=color, markersize=0)
+
     plt.show()
 
 def random_walk(x, y, step_size):
